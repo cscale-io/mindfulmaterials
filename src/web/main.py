7158d2d37fdc85e1bbff0bd54154764a77394c61
Jack Rusk <84130436+jackrusk@users.noreply.github.com>
2025-08-21 20:07:55 -0400

    add descriptions

--- src/web/main.py
+++ src/web/main.py
@@ -5,7 +5,20 @@ from pydantic import BaseModel, ValidationError
 from fastapi import FastAPI
 from src.web.routers import schema_router
 
-app = FastAPI(docs_url="/swagger")
+app = FastAPI(
+   title="Common Materials Schema",
+   version="BETA",
+   description=(
+       "A machine-readable implementation of the Common Materials Framework, "
+       "developed by [C.Scale](https://cscale.io) to facilitate the easy "
+       " exchange of product sustainability data across the building industry."
+   ),
+   contact={
+       "name": "Mindful Materials",
+       "url": "https://mindfulmaterials.com"
+   },
+   docs_url="/swagger"
+)
 
 app.mount("/", StaticFiles(directory="public", html=True), name="static")
 app.include_router(schema_router)
--- src/web/routers.py
+++ src/web/routers.py
@@ -2,8 +2,16 @@ from fastapi import APIRouter
 from src.entities.common_material_framework import CommonMaterialsSchema
 from src.services.validation import validate_data
 
-schema_router = APIRouter(prefix="/schema", tags=["schema"])
+schema_router = APIRouter(prefix="/schema", tags=["Data Checking"])
 
-@schema_router.post("/validate", response_model=CommonMaterialsSchema)
-async def validate_endpoint(data: CommonMaterialsSchema) -> CommonMaterialsSchema:
-    return await validate_data(data)
+@schema_router.post(
+   "/check", 
+   response_model=CommonMaterialsSchema,
+   description=(
+       "Check your data against the Common Materials Schema. "
+       "Use this endpoint to check that your input data is properly formatted "
+       "and aligns with the CMF structure before submitting or storing it."
+   )
+)
+async def check_your_data(data: CommonMaterialsSchema) -> CommonMaterialsSchema:
+   return await validate_data(data)
