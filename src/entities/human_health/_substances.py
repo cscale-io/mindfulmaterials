89a25d0c1862bdbae291455d8c1fb64eb88ad61c
Jack Rusk <84130436+jackrusk@users.noreply.github.com>
2025-09-05 21:53:17 -0400

    factor schemas for human health - substances

--- src/entities/human_health/_substances.py
+++ src/entities/human_health/_substances.py
@@ -1,36 +1,140 @@
-from pydantic import BaseModel
-from typing import Literal, Any
+from pydantic import BaseModel, Field
+from typing import Literal, Any, Optional, Enum, List
+from ..utilties import field_descriptions
 
 ###########################################
 # FACTORS
 ###########################################
 
+_factor_descriptions = {
+  "ReportingThreshold": "1.1.1.1 Reporting Threshold Level.",
+  "ThirdPartyVerification": "1.1.1.2 Third-Party Verified.",
+  "ExtentOfDisclosedInventory": "1.1.1.3 Extent of Disclosed Inventory.",
+  "Screening": "1.1.1.4 Screened. If true, 100% screened.",
+  "Characterization": "1.1.1.5 Characterized. If true, 100% characterized.",
+  "Identification": "1.1.1.6 Identified. If true, 100% identified.",
+  "ClassBasedSubstanceAvoidance": "1.1.4.1 Class-Based Substance Avoidance.",
+  "AssessmentBasedOptimization": "1.1.4.2 Assessment-Based Optimization.",
+  "RestrictedSubstanceListScreening": "1.1.4.3 Restricted Substance List (RSL) Screening Compliance."
+}
+
+# Reporting Threshold
+
+class ReportingThresholdValue(Enum):
+    PPM_100 = "100 ppm"
+    PPM_1000 = "1,000 ppm" 
+    PPM_10000 = "10,000 ppm"
+
 class ReportingThreshold(BaseModel):
-   value: Any
+    metric: Optional[ReportingThresholdValue] = Field(None, description=field_descriptions['metric'])
+    description: str = Field(_factor_descriptions['ReportingThreshold'], 
+                           description=field_descriptions['description'])
+    standard: Any = Field(None, description=field_descriptions['standard'])
+
+# Third-Party Verification
 
 class ThirdPartyVerification(BaseModel):
-   value: Any
+    metric: Optional[bool] = Field(None, description=field_descriptions['metric'])
+    description: str = Field(_factor_descriptions['ThirdPartyVerification'], 
+                           description=field_descriptions['description'])
+    standard: Any = Field(None, description=field_descriptions['standard'])
 
 class ExtentOfDisclosedInventory(BaseModel):
    value: Any
 
+# Screened?
+
 class Screening(BaseModel):
-   value: Any
+   metric: Optional[bool] = Field(None, description=field_descriptions['metric'])
+   description: str = Field(_factor_descriptions['Screening'], 
+                          description=field_descriptions['description'])
+   standard: Any = Field(None, description=field_descriptions['standard'])
+
+# Characterized?
 
 class Characterization(BaseModel):
-   value: Any
+   metric: Optional[bool] = Field(None, description=field_descriptions['metric'])
+   description: str = Field(_factor_descriptions['Characterization'], 
+                          description=field_descriptions['description'])
+   standard: Any = Field(None, description=field_descriptions['standard'])
+
+# Identified?
 
 class Identification(BaseModel):
-   value: Any
+   metric: Optional[bool] = Field(None, description=field_descriptions['metric'])
+   description: str = Field(_factor_descriptions['Identification'], 
+                          description=field_descriptions['description'])
+   standard: Any = Field(None, description=field_descriptions['standard'])
+
+# Class-Based Substance Avoidance
+
+class AvoidedSubstance(Enum):
+   NO_ANTIMICROBIALS = "No Antimicrobials"
+   NO_BISPHENOLS = "No Bisphenols"
+   NO_FLAME_RETARDANTS = "No Flame Retardants"
+   NO_FORMALDEHYDES = "No Formaldehydes"
+   NO_ORTHO_PHTHALATES = "No Ortho-Phthalates"
+   NO_PFAS = "No PFAS"
+   NO_PVC_CPVC = "No Polyvinyl Chloride or Chlorinated Polyvinyl Chloride"
+   NO_ALKYLPHENOLS = "No Alkylphenols"
+   NO_CHLOROPHENOLS = "No Chlorophenols"
+   NO_ISOCYANATES = "No Isocyanates"
+   NO_METHYL_ACRYLATE = "No Methyl & Acrylate Groups"
+   NO_TOXIC_METALS = "No Toxic Metals & Compounds"
+   NO_STYRENE_BUTADIENE = "No Styrene Butadiene Rubber"
+   NO_CRUMB_RUBBER = "No Crumb Rubber"
+   NO_FLY_ASH = "No Fly Ash"
+   NO_AZO_DYES = "No Azo Dyes"
+
+# Class-Based Substance Avoidance
 
 class ClassBasedSubstanceAvoidance(BaseModel):
-   value: Any
+   metric: Optional[List[AvoidedSubstance]] = Field(None, description=field_descriptions['metric'])
+   description: str = Field(_factor_descriptions['ClassBasedSubstanceAvoidance'], 
+                           description=field_descriptions['description'])
+   standard: Any = Field(None, description=field_descriptions['standard'])
+
+# Assessment-Based Optimization
+
+class OptimizationPercentage(Enum):
+   HUNDRED_PERCENT = "100%"
+   NINETY_FIVE_PERCENT = "95%"
+   SEVENTY_FIVE_PERCENT = "75%"
 
 class AssessmentBasedOptimization(BaseModel):
-   value: Any
+   metric: Optional[OptimizationPercentage] = Field(None, description=field_descriptions['metric'])
+   description: str = Field(_factor_descriptions['AssessmentBasedOptimization'], 
+                        description=field_descriptions['description'])
+   standard: Any = Field(None, description=field_descriptions['standard'])
+
+# Restricted Substance List (RSL) Screening Compliance
+
+class RestrictedSubstanceList(Enum):
+   GREENSCREEN_BM1_LT1 = "GreenScreen BM-1 or LT-1 Chemicals"
+   REACH_SVHC = "REACH Candidate List of Substances of Very High Concern (SVHC)"
+   LBC_RED_LIST_V4_APR2024 = "Living Building Challenge Red List Substances - v4.0 (April 2024 Update)"
+   LBC_RED_LIST_V3_1 = "Living Building Challenge Red List Substances - v3.1"
+   C2C_RESTRICTED_V4 = "Cradle to Cradle Restricted Substances (version 4)"
+   C2C_BANNED_V3 = "Cradle to Cradle Banned List Substances (version 3)"
+   SIN_LIST = "SIN List"
+   GSPI_SIX_CLASSES = "Green Science Policy Institute Six Classes"
+   PERKINS_WILL_PRECAUTIONARY = "Perkins+Will Precautionary List"
+   KAISER_EPP_CHEMICALS = "Kaiser Permanente EPP Chemicals of Concern"
+   CA_EPA_PROP65 = "CA EPA - Prop 65"
+   EPA_PRIORITY_PBTS = "US EPA - Priority PBTs (PPT)"
+   EPA_TRI_PBTS = "US EPA - Toxics Release Inventory PBTs"
+   LBC_RED_LIST_V4_MAR2022 = "Living Building Challenge Red List Substances - v4.0 (March 2022 Update)"
+   HCWH_HEALTHY_INTERIORS = "HCWH Healthy Interiors: Furniture & Furnishings, v2.4"
+   REACH_AUTHORIZATION = "REACH Authorization List (Annex XIV)"
+   REACH_RESTRICTIONS = "REACH Restrictions List (Annex XVII)"
+   LBC_RED_LIST_V4_APR2023 = "Living Building Challenge Red List Substances - v4.0 (April 2023 Update)"
+   ROHS_RESTRICTED = "RoHS Restricted Substances, EU (ANNEX II, Version 2.0)"
 
 class RestrictedSubstanceListScreening(BaseModel):
-   value: Any
+   metric: Optional[List[RestrictedSubstanceList]] = Field(None, description=field_descriptions['metric'])
+   description: str = Field(_factor_descriptions['RestrictedSubstanceListScreening'], 
+                          description=field_descriptions['description'])
+   standard: Any = Field(None, description=field_descriptions['standard'])
 
 ###########################################
 # CATEGORIES
